16cfe4f74f3787c6b9b32ac8f759f350c2b49b72
tailatitude <13520730@gm.uit.edu.vn>
2016-06-16 00:21:08 +0700

    init proj

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tailatitude
+ * Date: 6/15/2016
+ * Time: 10:18 PM
+ */
+echo "test page";
+echo "test git";
+echo "fycj"
+?>
---
+++ viewpost.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tailatitude
+ * Date: 6/15/2016
+ * Time: 10:18 PM
+ */
